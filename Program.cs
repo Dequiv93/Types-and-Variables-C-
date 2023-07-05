e47540d663250e1b687f02ed9f6faa3dd2b194fa
Denis Quiros-Vega <albertoquive1993@gmail.com>
2023-07-04 23:16:42 -0600

    Types_Variables_C#

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿bool active = true;
+active = false;
+Console.WriteLine(active);
+
+
+byte size = 100;
+size = 0;
+Console.WriteLine(size);
+
+
+char initial = 'D';
+initial = 'A'; 
+Console.WriteLine(initial);
+
+short length = 20000;
+length = 5000;
+Console.WriteLine(length);
+
+
+int age = 29;
+age = 30;
+Console.WriteLine(age);
+
+float microDistance = 1.233F;
+microDistance = 2.545F;
+Console.WriteLine(microDistance);
+
+double grade = 90.50;
+grade = 12.75;
+Console.WriteLine(grade);
+
+long distance = 500000000000;
+distance = 6000000000; 
+Console.WriteLine(distance);
+
+decimal measurement = 1.45646598653612313246548479561461465M;
+measurement = 3.6561546958989654694646549879464646545M;
+Console.WriteLine(measurement);
+
+string lastname = "Quiros";
+lastname = "Vega";
+Console.WriteLine(lastname);
+
+
+
+
+
+
+
+
+
+
